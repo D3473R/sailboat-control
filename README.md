b934bb5e12a904d6360d179e04aa06d39a43bd3d
D3473R <d3473r@gmail.com>
2019-08-03 18:26:12 +0200

    Use custom message definition

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # sailboat-control
+
+## Installation
+
++ `cd ${HOME}`
++ `git clone --recursive https://github.com/D3473R/sailboat-control.git`
++ `cd sailboat-control`
++ `pipenv install`
++ `pipenv shell`
++ `MDEF=${HOME}/sailboat-control/sailboatcontrol/message_definitions/ pip install sailboatcontrol/pymavlink/ -v`
++ `exit`
+
+Replace in `sailboatcontrol/pymavlink/generator/mavgen_typescript.py` every `node-mavlink` with `@ifrunistuttgart/node-mavlink`
+
+## Generate new mavlink library files
+
+### For [sailboat-control](https://github.com/D3473R/sailboat-control)
+
++ Edit `sailboatcontrol/message_definitions/v1.0/sailboat.xml`
++ Remove `sailboatcontrol/pymavlink/dialects/v20/sailboat.py`
++ Run `pipenv run python sailboatcontrol/control.py`
+
+### For [sailboat-gcs](https://github.com/D3473R/sailboat-gcs)
+
++ Edit `sailboatcontrol/message_definitions/v1.0/sailboat.xml`
++ `mkdir mavlink`
++ `python sailboatcontrol/pymavlink/tools/mavgen.py -o ./mavlink --lang TypeScript --wire-protocol 2.0 sailboatcontrol/message_definitions/v1.0/sailboat.xml`
++ Move the `mavlink` folder to `sailboat-gcs/src/assets/`
--- sailboatcontrol/mavlink.py
+++ sailboatcontrol/mavlink.py
@@ -1,20 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import math
 import time
 
 from setup_logger import logging
 from threading import Thread, Event
+
+os.environ['MAVLINK20'] = '1'
+os.environ['MAVLINK_DIALECT'] = 'sailboat'
+
 from pymavlink import mavutil
-from pymavlink.dialects.v20 import common as mavlink
+import pymavlink.dialects.v20.sailboat as mavlink
 
 MAVLINK_SERIAL_PORT = '/dev/ttyUSB0'
 MAVLINK_SERIAL_BAUD = 57600  # This should be set to 57600 in order to work with the Holybro Telemetry Radio V3
-UPDATE_SLEEP = 0.5
+UPDATE_SLEEP = 1
 
 
 class Mavlink(Thread):
     def __init__(self, stop_event, store):
-        """ The mavlink telemetry class. """
+        ''' The mavlink telemetry class. '''
 
         Thread.__init__(self, name='Mavlink')
         self.stop_event = stop_event
@@ -23,21 +30,19 @@ class Mavlink(Thread):
         self.send_thread_stop = Event()
         self.receive_thread_stop = Event()
         self.heartbeat_thread_stop = Event()
-        self.connection = mavutil.mavlink_connection(MAVLINK_SERIAL_PORT, dialect='common', baud=MAVLINK_SERIAL_BAUD)
+        self.connection = mavutil.mavlink_connection(MAVLINK_SERIAL_PORT, dialect='sailboat', baud=MAVLINK_SERIAL_BAUD)
         self.send_thread = Thread(target=self.send, name='Mavlink-send')
         self.receive_thread = Thread(target=self.receive, name='Mavlink-receive')
         self.heartbeat_thread = Thread(target=self.heartbeat, name='Mavlink-heartbeat')
 
-        os.environ["MAVLINK20"] = "1"
-
     def run(self):
-        logging.info("Starting send thread")
+        logging.info('Starting send thread')
         self.send_thread.start()
 
-        logging.info("Starting receive thread")
+        logging.info('Starting receive thread')
         self.receive_thread.start()
 
-        logging.info("Starting heartbeat thread")
+        logging.info('Starting heartbeat thread')
         self.heartbeat_thread.start()
 
         while not self.stop_event.is_set():
@@ -47,13 +52,15 @@ class Mavlink(Thread):
 
     def send(self):
         while not self.send_thread_stop.is_set():
+            gps = self.store.__getitem__('gps')
             compass = self.store.__getitem__('compass')
-            message = self.connection.mav.attitude_encode(
-                0,
-                math.radians(compass['roll']),
-                math.radians(compass['pitch']),
-                math.radians(compass['heading']),
-                1, 1, 1
+            message = self.connection.mav.boat_status_encode(
+                float(compass['roll']),
+                float(compass['pitch']),
+                float(compass['heading']),
+                float(gps['lat']) * 10000000,
+                float(gps['lon']) * 10000000,
+                float(gps['speed'])
             )
             self.connection.write(message.pack(self.connection.mav))
             logging.info('sending: {}'.format(message))
@@ -65,7 +72,7 @@ class Mavlink(Thread):
         while not self.receive_thread_stop.is_set():
             m = self.connection.recv_match(blocking=False)
             if m is not None:
-                logging.info('receiving: {}'.format(m))
+                logging.debug('receiving: {}'.format(m))
 
     def heartbeat(self):
         last_heartbeat_send = time.time()
@@ -81,7 +88,7 @@ class Mavlink(Thread):
                 )
                 self.connection.write(heartbeat.pack(self.connection.mav))
                 last_heartbeat_send = now
-                logging.info('sending: {}'.format(heartbeat))
+                logging.debug('sending: {}'.format(heartbeat))
                 logging.debug(
                     'sending hex: {}'.format(' '.join(hex(i) for i in bytearray(heartbeat.pack(self.connection.mav)))))
 
